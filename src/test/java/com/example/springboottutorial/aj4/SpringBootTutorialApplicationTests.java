32b3bccc4f294026618caf429a8fab26b0af6c70
Andrei Pokhilko <andrei.pokhilko@aspekt.com.mk>
2021-10-07 18:13:49 +0600

    initial commit, working application

---
+++ src/test/java/com/example/springboottutorial/aj4/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottutorial.aj4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
