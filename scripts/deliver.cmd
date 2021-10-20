19abbfcbac078928120acbd6cf4d0b79c1399a76
Andrei Pokhilko <andrei.pokhilko@aspekt.com.mk>
2021-10-20 20:35:04 +0600

    add version to index.html fix path in deliver.cmd

--- scripts/deliver.cmd
+++ scripts/deliver.cmd
@@ -1,3 +1,3 @@
-scp ..\target\spring-boot-tutorial-aj4-0.0.1-SNAPSHOT.jar root@49.12.229.158:/var/lib/spring-boot-tutorial-aj4.jar
+scp ..\target\spring-boot-tutorial-aj4-0.0.1-SNAPSHOT.jar root@49.12.229.158:/var/lib/spring-boot-tutorial-aj4/spring-boot-tutorial-aj4.jar
 ssh root@49.12.229.158 systemctl restart spring-boot-tutorial-aj4.service
 pause
