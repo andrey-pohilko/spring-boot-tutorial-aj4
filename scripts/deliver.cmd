1f5497a140410831410c323c699ad90e8cd1e995
Andrei Pokhilko <andrei.pokhilko@aspekt.com.mk>
2021-10-20 19:36:46 +0600

    script and text notes for tutorial

---
+++ scripts/deliver.cmd
@@ -0,0 +1,3 @@
+scp ..\target\spring-boot-tutorial-aj4-0.0.1-SNAPSHOT.jar root@23.88.101.229:/var/lib/spring-boot-tutorial-aj4.jar
+ssh root@23.88.101.229 systemctl restart spring-boot-tutorial-aj3.service
+pause
